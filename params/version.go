84267d69d46fe08061203baae4b324769c6c00ad
Sudeep <sudeepdino008@gmail.com>
2023-03-06 14:44:29 +0530

    updates bsp version to 1.4.0

--- params/version.go
+++ params/version.go
@@ -29,8 +29,8 @@ const (
 
 const (
 	BspVersionMajor = 1 // Major version component of the current release
-	BspVersionMinor = 3 // Minor version component of the current release
-	BspVersionPatch = 6 // Patch version component of the current release
+	BspVersionMinor = 4 // Minor version component of the current release
+	BspVersionPatch = 0 // Patch version component of the current release
 )
 
 // Version holds the textual version string.
